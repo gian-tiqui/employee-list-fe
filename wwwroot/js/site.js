daaf317f0a50fc8bed1ac5c0bf7ca517e4ea3757
gian <michaelgiantiqui3@gmail.com>
2023-12-22 19:35:27 +0800

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿const user = JSON.parse(localStorage.getItem("user"));
+
+if (user) {
+    $("#loginBtn").hide();
+    $("#logoutBtn").show();
+    $("#employeesBtn").show();
+} else {
+    $("#loginBtn").show();
+    $("#logoutBtn").hide();
+    $("#employeesBtn").hide();
+}
+
+$("#logoutBtn").on("click", () => {
+    localStorage.clear();
+    window.location.href = "/";
+});
